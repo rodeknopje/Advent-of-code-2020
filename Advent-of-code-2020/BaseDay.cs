09c073f42d36775757355c930f6528723e4147a2
Rodeknopje <mn.stokhorst@gmail.com>
2020-12-02 16:58:58 +0100

    Initial commit

---
+++ Advent-of-code-2020/BaseDay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCode_2020
+{
+    public abstract class BaseDay
+    {
+        protected abstract int Day { get; }
+
+        public abstract void Solution1();
+        public abstract void Solution2();
+
+        protected string FilePath =>  Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName,$"input\\{Day}.txt");
+    }
+}
---
+++ Advent-of-code-2020/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode_2020
+{
+    static class Program
+    {
+        public static void Main()
+        {
+            new Day1().Solution1();
+            new Day1().Solution2();
+            new Day2().Solution1();
+            new Day2().Solution2();
+            new Day3().Solution1();
+            new Day3().Solution2();
+        }
+    }
+}
