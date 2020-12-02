91c770fbfbc0373a443d73777ac0b2c5c0fa42bf
Rodeknopje <mn.stokhorst@gmail.com>
2020-12-02 16:59:10 +0100

    Solutions day 1

---
+++ Advent-of-code-2020/Day1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode_2020
+{
+    public class Day1 : BaseDay
+    {
+        protected override int Day => 1;
+
+        private List<int> Numbers { get; set; }
+
+        public Day1()
+        {
+            Numbers = File.ReadAllLines(FilePath).Select(line => Convert.ToInt32(line)).ToList();
+        }
+
+        public override void Solution1()
+        {
+            Console.WriteLine((
+                from a in Numbers 
+                from b in Numbers 
+                where a + b == 2020 
+                select a * b).FirstOrDefault());
+        }
+
+        public override void Solution2()
+        {
+            Console.WriteLine((
+                from a in Numbers
+                from b in Numbers
+                from c in Numbers
+                where a + b + c == 2020
+                select a * b * c).FirstOrDefault());
+        }
+    }
+}
