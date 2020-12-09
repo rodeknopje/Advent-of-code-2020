5b37ea3b913e3a61acd4950bd4441a2d04f5c57e
Rodeknopje <mn.stokhorst@gmail.com>
2020-12-09 20:28:39 +0100

    solved day 6

---
+++ Advent-of-code-2020/Day6.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Advent_of_code_2020
+{
+    public class Day6 : BaseDay
+    {
+        protected override int Day => 6;
+
+        public override void Solution1()
+        {
+            Console.WriteLine(
+                File.ReadAllText(FilePath)
+                    .Split("\r\n\r\n").ToList()
+                    .Select(group => group
+                        .Replace("\r\n", string.Empty)
+                        .Distinct()
+                        .Count())
+                    .Sum()
+            );
+        }
+
+        public override void Solution2()
+        {
+            var groups = File.ReadAllText(FilePath)
+                .Split("\r\n\r\n")
+                .Select(group => group.Split("\r\n").ToList()).ToList();
+
+            var answer = 0;
+            
+            foreach (var group in groups)
+            {
+                foreach (var letter in "abcdefghijklmnopqrstuvwxyz")
+                {
+                    var letterCount = group.Count(line => line.Contains(letter));
+                    
+                    if (letterCount == group.Count)
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
--- Advent-of-code-2020/Program.cs
+++ Advent-of-code-2020/Program.cs
@@ -11,8 +11,10 @@ namespace Advent_of_code_2020
             // new Day2().Solution2();
             // new Day4().Solution1();
             // new Day4().Solution2();           
-            new Day5().Solution1();
-            new Day5().Solution2();
+            // new Day5().Solution1();
+            // new Day5().Solution2();            
+            new Day6().Solution1();
+            new Day6().Solution2();
         }
     }
 }
